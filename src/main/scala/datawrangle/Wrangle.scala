687762d74150a845df3aeca385d4389781b9d17f
Chandler May <cmay14@jhu.edu>
2013-09-10 08:29:48 -0400

    change wrangle hard-coded paths aclemmer -> cmay

--- src/main/scala/datawrangle/Wrangle.scala
+++ src/main/scala/datawrangle/Wrangle.scala
@@ -108,8 +108,8 @@ object Text {
 }
 
 object DataConsts {
-  val RESULTS_DIR = "/home/hltcoe/aclemmer/slda/results/"
-  val DATA_DIR = "/home/hltcoe/aclemmer/slda/data/"
+  val RESULTS_DIR = "/home/hltcoe/cmay/slda/results/"
+  val DATA_DIR = "/home/hltcoe/cmay/slda/data/"
   val TNG_TRAIN_DIR = DATA_DIR + "20news-bydate-train/"
   val TNG_WHITELIST = DATA_DIR + "TNG_WHITELIST"
   val TNG_STOP_WORDS = DATA_DIR + "TNG_STOP_WORDS"
