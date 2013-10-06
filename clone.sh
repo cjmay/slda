16c0d59681b270e2fa1b8651a320af6cb9104370
Chandler May <cjmay@jhu.edu>
2013-10-06 15:28:01 -0400

    add clone script

---
+++ clone.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+ROOT_REPO_PATH="$PWD"
+
+NEW_REPO_PATH_STEM="$HOME/slda-run"
+i=0
+new_repo_path="$NEW_REPO_PATH_STEM-$i"
+while [ -d "$new_repo_path" ]
+do
+    i=$(($i + 1))
+    new_repo_path="$NEW_REPO_PATH_STEM-$i"
+done
+
+echo "Cloning $ROOT_REPO_PATH to $new_repo_path"
+echo -n "Proceed (y/n)? "
+answer=`read -n 1`
+echo
+
+if [ "$answer" == "y" ]
+then
+    git clone "$ROOT_REPO_PATH" "$new_repo_path"
+    ln -s "$ROOT_REPO_PATH/data/20news-bydate-train" "$new_repo_path/"
+    ln -s "$ROOT_REPO_PATH/data/20news-bydate-test" "$new_repo_path/"
+fi
