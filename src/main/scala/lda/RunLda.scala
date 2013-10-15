5bde64957c9fd74a4d827198cfa58e39ad15551a
Chandler May <cjmay4754@gmail.com>
2013-10-15 01:00:00 -0400

    update runlda comments, leave params rather arbitrary

--- src/main/scala/lda/RunLda.scala
+++ src/main/scala/lda/RunLda.scala
@@ -8,72 +8,72 @@ import wrangle._
 object Sim3PfParams {
   val alpha = 0.1 // topic distribution prior
   val beta = 0.1 // word distribution prior
-  val reservoirSize = 176//8 // number of documents (total: 1768)
+  val reservoirSize = 1768 // reservoir size in tokens
   val numParticles = 100
   val ess = 20 // effective sample size threshold
   val rejuvBatchSize = 30 // |R(i)|
   val rejuvMcmcSteps = 20
-  val initialBatchSize = 1//77
+  val initialBatchSize = 10 // number of docs for batch MCMC init
   val initialBatchMcmcSteps = 2000
 }
 
 object Rel3PfParams {
   val alpha = 0.1 // topic distribution prior
   val beta = 0.1 // word distribution prior
-  val reservoirSize = 1575 // number of documents (total: 1575)
+  val reservoirSize = 1575 // reservoir size in tokens
   val numParticles = 100
   val ess = 20 // effective sample size threshold
   val rejuvBatchSize = 30 // |R(i)|
   val rejuvMcmcSteps = 20
-  val initialBatchSize = 158
+  val initialBatchSize = 158 // number of docs for batch MCMC init
   val initialBatchMcmcSteps = 2000
 }
 
 object Diff3PfParams {
   val alpha = 0.1 // topic distribution prior
   val beta = 0.1 // word distribution prior
-  val reservoirSize = 1670 // number of documents (total: 1670)
+  val reservoirSize = 1670 // reservoir size in tokens
   val numParticles = 100
   val ess = 10 // effective sample size threshold
   val rejuvBatchSize = 10 // |R(i)|
   val rejuvMcmcSteps = 20
-  val initialBatchSize = 167
+  val initialBatchSize = 167 // number of docs for batch MCMC init
   val initialBatchMcmcSteps = 2000
 }
 
 object Subset20PfParams {
   val alpha = 0.1 // topic distribution prior
   val beta = 0.1 // word distribution prior
-  val reservoirSize = 0 // number of documents
+  val reservoirSize = 0 // reservoir size in tokens
   val numParticles = 100
   val ess = 10 // effective sample size threshold
   val rejuvBatchSize = 10 // |R(i)|
   val rejuvMcmcSteps = 20
-  val initialBatchSize = 0
+  val initialBatchSize = 0 // number of docs for batch MCMC init
   val initialBatchMcmcSteps = 2000
 }
 
 object Slash6PfParams {
   val alpha = 0.1 // topic distribution prior
   val beta = 0.1 // word distribution prior
-  val reservoirSize = 0 // number of documents
+  val reservoirSize = 0 // reservoir size in tokens
   val numParticles = 100
   val ess = 10 // effective sample size threshold
   val rejuvBatchSize = 10 // |R(i)|
   val rejuvMcmcSteps = 20
-  val initialBatchSize = 0
+  val initialBatchSize = 0 // number of docs for batch MCMC init
   val initialBatchMcmcSteps = 2000
 }
 
 object Slash7PfParams {
   val alpha = 0.1 // topic distribution prior
   val beta = 0.1 // word distribution prior
-  val reservoirSize = 0 // number of documents
+  val reservoirSize = 0 // reservoir size in tokens
   val numParticles = 100
   val ess = 20 // effective sample size threshold
   val rejuvBatchSize = 30 // |R(i)|
   val rejuvMcmcSteps = 20
-  val initialBatchSize = 0
+  val initialBatchSize = 0 // number of docs for batch MCMC init
   val initialBatchMcmcSteps = 2000
 }
 
@@ -92,6 +92,7 @@ object RunLda {
                           Sim3PfParams.rejuvMcmcSteps)
 
     // TODO: batch size: documents...? not tokens?
+    // TODO: what if batch size is bigger than corpus?
     println("initializing...")
     model.initialize(
       (0 to Sim3PfParams.initialBatchSize-1).map(corpus(_)).toArray,
