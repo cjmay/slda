8d795cb8d5adac8c81294b034ce4993da34a07a5
Chandler May <cjmay4754@gmail.com>
2013-10-15 12:25:32 -0400

    switch to diff3

--- src/main/scala/lda/RunLda.scala
+++ src/main/scala/lda/RunLda.scala
@@ -37,7 +37,7 @@ object Diff3PfParams {
   val ess = 10 // effective sample size threshold
   val rejuvBatchSize = 10 // |R(i)|
   val rejuvMcmcSteps = 20
-  val initialBatchSize = 167 // number of docs for batch MCMC init
+  val initialBatchSize = 1670 // number of docs for batch MCMC init
   val initialBatchMcmcSteps = 2000
 }
 
@@ -80,32 +80,32 @@ object Slash7PfParams {
 object RunLda {
   def main (args: Array[String]) {
     println("loading corpus...")
-    var (corpus, labels, cats) = wrangle.TNG.sim3
+    var (corpus, labels, cats) = wrangle.TNG.diff3
     // if we don't shuffle them, and if we don't shuffle them with the
     // same seed, our NMI suffers greatly
     corpus = (new Random(10)).shuffle(corpus.toSeq).toArray
     labels = (new Random(10)).shuffle(labels.toSeq).toArray
 
     println("initializing model...")
-    val model = new PfLda(cats, Sim3PfParams.alpha, Sim3PfParams.beta,
-                          Sim3PfParams.reservoirSize, Sim3PfParams.numParticles,
-                          Sim3PfParams.ess, Sim3PfParams.rejuvBatchSize,
-                          Sim3PfParams.rejuvMcmcSteps)
+    val model = new PfLda(cats, Diff3PfParams.alpha, Diff3PfParams.beta,
+                          Diff3PfParams.reservoirSize, Diff3PfParams.numParticles,
+                          Diff3PfParams.ess, Diff3PfParams.rejuvBatchSize,
+                          Diff3PfParams.rejuvMcmcSteps)
 
     val evaluate = (docIdx: Int) =>
       Evaluation.writeOut(model, labels.slice(0, docIdx+1),
-                          DataConsts.SIM_3_LABELS.slice(0, docIdx+1),
+                          DataConsts.DIFF_3_LABELS.slice(0, docIdx+1),
                           DataConsts.RESULTS_DIR +  docIdx.toString() + ".txt")
 
     // TODO: batch size: documents...? not tokens?
     // TODO: what if batch size is bigger than corpus?
     model.initialize(
-      (0 to Sim3PfParams.initialBatchSize-1).map(corpus(_)).toArray,
-      Sim3PfParams.initialBatchMcmcSteps,
+      (0 to Diff3PfParams.initialBatchSize-1).map(corpus(_)).toArray,
+      Diff3PfParams.initialBatchMcmcSteps,
       evaluate)
 
     println("running particle filter...")
-    for (i <- Sim3PfParams.initialBatchSize to corpus.length-1) {
+    for (i <- Diff3PfParams.initialBatchSize to corpus.length-1) {
       println("DOCUMENT " + i + " / " + corpus.length)
       //val now = System.nanoTime
       model.ingestDoc(corpus(i))
@@ -116,7 +116,7 @@ object RunLda {
     }
     model.writeTopics("results.txt")
 
-    val mis = Evaluation.nmi(model, labels, wrangle.DataConsts.SIM_3_LABELS)
+    val mis = Evaluation.nmi(model, labels, wrangle.DataConsts.DIFF_3_LABELS)
     println(mis.deep)
   }
 }
