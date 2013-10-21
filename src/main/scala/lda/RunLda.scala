9b219b5648e4f3683aa5d2cd85f8ecac7ea6d7b2
Chandler May <cjmay4754@gmail.com>
2013-10-20 22:24:26 -0400

    abstract away run lda params

--- src/main/scala/lda/RunLda.scala
+++ src/main/scala/lda/RunLda.scala
@@ -5,107 +5,94 @@ import scala.util.Random
 import evaluation._
 import wrangle._
 
-object Sim3PfParams {
+trait RunLdaParams {
+  val alpha: Double
+  val beta: Double
+  val reservoirSize: Int
+  val numParticles: Int
+  val ess: Double
+  val rejuvBatchSize: Int
+  val rejuvMcmcSteps: Int
+  val initialBatchSize: Int
+  val initialBatchMcmcSteps: Int
+  val labelTypes: List[String]
+  val corpus: Array[String]
+  val labels: Array[String]
+  val cats: Int
+}
+
+object Sim3PfParams extends RunLdaParams {
   val alpha = 0.1 // topic distribution prior
   val beta = 0.1 // word distribution prior
   val reservoirSize = 1768 // reservoir size in tokens
   val numParticles = 100
-  val ess = 20 // effective sample size threshold
+  val ess = 20.0 // effective sample size threshold
   val rejuvBatchSize = 30 // |R(i)|
   val rejuvMcmcSteps = 20
   val initialBatchSize = 177 // number of docs for batch MCMC init
-  val initialBatchMcmcSteps = 2000
+  val initialBatchMcmcSteps = 200
+  val labelTypes = wrangle.DataConsts.SIM_3_LABELS
+  val (corpus, labels, cats) = wrangle.TNG.sim3
 }
 
-object Rel3PfParams {
+object Rel3PfParams extends RunLdaParams {
   val alpha = 0.1 // topic distribution prior
   val beta = 0.1 // word distribution prior
   val reservoirSize = 1575 // reservoir size in tokens
   val numParticles = 100
-  val ess = 20 // effective sample size threshold
+  val ess = 20.0 // effective sample size threshold
   val rejuvBatchSize = 30 // |R(i)|
   val rejuvMcmcSteps = 20
   val initialBatchSize = 158 // number of docs for batch MCMC init
-  val initialBatchMcmcSteps = 2000
+  val initialBatchMcmcSteps = 200
+  val labelTypes = wrangle.DataConsts.REL_3_LABELS
+  val (corpus, labels, cats) = wrangle.TNG.rel3
 }
 
-object Diff3PfParams {
+object Diff3PfParams extends RunLdaParams {
   val alpha = 0.1 // topic distribution prior
   val beta = 0.1 // word distribution prior
   val reservoirSize = 1670 // reservoir size in tokens
   val numParticles = 100
-  val ess = 10 // effective sample size threshold
+  val ess = 20.0 // effective sample size threshold
   val rejuvBatchSize = 10 // |R(i)|
   val rejuvMcmcSteps = 20
-  val initialBatchSize = 1670 // number of docs for batch MCMC init
-  val initialBatchMcmcSteps = 2000
-}
-
-object Subset20PfParams {
-  val alpha = 0.1 // topic distribution prior
-  val beta = 0.1 // word distribution prior
-  val reservoirSize = 0 // reservoir size in tokens
-  val numParticles = 100
-  val ess = 10 // effective sample size threshold
-  val rejuvBatchSize = 10 // |R(i)|
-  val rejuvMcmcSteps = 20
-  val initialBatchSize = 0 // number of docs for batch MCMC init
-  val initialBatchMcmcSteps = 2000
-}
-
-object Slash6PfParams {
-  val alpha = 0.1 // topic distribution prior
-  val beta = 0.1 // word distribution prior
-  val reservoirSize = 0 // reservoir size in tokens
-  val numParticles = 100
-  val ess = 10 // effective sample size threshold
-  val rejuvBatchSize = 10 // |R(i)|
-  val rejuvMcmcSteps = 20
-  val initialBatchSize = 0 // number of docs for batch MCMC init
-  val initialBatchMcmcSteps = 2000
-}
-
-object Slash7PfParams {
-  val alpha = 0.1 // topic distribution prior
-  val beta = 0.1 // word distribution prior
-  val reservoirSize = 0 // reservoir size in tokens
-  val numParticles = 100
-  val ess = 20 // effective sample size threshold
-  val rejuvBatchSize = 30 // |R(i)|
-  val rejuvMcmcSteps = 20
-  val initialBatchSize = 0 // number of docs for batch MCMC init
-  val initialBatchMcmcSteps = 2000
+  val initialBatchSize = 167 // number of docs for batch MCMC init
+  val initialBatchMcmcSteps = 200
+  val labelTypes = wrangle.DataConsts.DIFF_3_LABELS
+  val (corpus, labels, cats) = wrangle.TNG.diff3
 }
 
 object RunLda {
   def main (args: Array[String]) {
+    val params = Diff3PfParams
+
     println("loading corpus...")
-    var (corpus, labels, cats) = wrangle.TNG.diff3
     // if we don't shuffle them, and if we don't shuffle them with the
     // same seed, our NMI suffers greatly
-    corpus = (new Random(10)).shuffle(corpus.toSeq).toArray
-    labels = (new Random(10)).shuffle(labels.toSeq).toArray
+    val corpus = (new Random(10)).shuffle(params.corpus.toSeq).toArray
+    val labels = (new Random(10)).shuffle(params.labels.toSeq).toArray
 
     println("initializing model...")
-    val model = new PfLda(cats, Diff3PfParams.alpha, Diff3PfParams.beta,
-                          Diff3PfParams.reservoirSize, Diff3PfParams.numParticles,
-                          Diff3PfParams.ess, Diff3PfParams.rejuvBatchSize,
-                          Diff3PfParams.rejuvMcmcSteps)
+    val model = new PfLda(params.cats, params.alpha, params.beta,
+                          params.reservoirSize, params.numParticles,
+                          params.ess, params.rejuvBatchSize,
+                          params.rejuvMcmcSteps)
 
     val evaluate = (docIdx: Int) =>
       Evaluation.writeOut(model, labels.slice(0, docIdx+1),
-                          DataConsts.DIFF_3_LABELS.slice(0, docIdx+1),
+                          params.labelTypes.slice(0, docIdx+1),
                           DataConsts.RESULTS_DIR +  docIdx.toString() + ".txt")
 
     // TODO: batch size: documents...? not tokens?
     // TODO: what if batch size is bigger than corpus?
     model.initialize(
-      (0 to Diff3PfParams.initialBatchSize-1).map(corpus(_)).toArray,
-      Diff3PfParams.initialBatchMcmcSteps,
+      (0 to params.initialBatchSize-1).map(corpus(_)).toArray,
+      params.initialBatchMcmcSteps,
       evaluate)
 
     println("running particle filter...")
-    for (i <- Diff3PfParams.initialBatchSize to corpus.length-1) {
+    for (i <- params.initialBatchSize to corpus.length-1) {
       println("DOCUMENT " + i + " / " + corpus.length)
       //val now = System.nanoTime
       model.ingestDoc(corpus(i))
@@ -116,7 +103,7 @@ object RunLda {
     }
     model.writeTopics("results.txt")
 
-    val mis = Evaluation.nmi(model, labels, wrangle.DataConsts.DIFF_3_LABELS)
+    val mis = Evaluation.nmi(model, labels, params.labelTypes)
     println(mis.deep)
   }
 }
