db524700abb2f87728a32aff2fa8ca86ba71675a
Chandler May <cjmay4754@gmail.com>
2013-10-23 11:09:17 -0400

    no gsed... sigh...

--- clone3.sh
+++ clone3.sh
@@ -8,7 +8,7 @@ for params_name_prefix in Diff3 Rel3 Sim3
 do
     new_repo_path=`bash clone.sh`
     echo "$new_repo_path"
-    gsed -i '/^[[:space:]]*val params: RunLdaParams =.*$/s/=.*$/'"= ${params_name_prefix}${params_name_stem}/" "$new_repo_path/$params_rel_path"
+    sed -i '/^[[:space:]]*val params: RunLdaParams =.*$/s/=.*$/'"= ${params_name_prefix}${params_name_stem}/" "$new_repo_path/$params_rel_path"
     pushd "$new_repo_path" >/dev/null
     if [ $# -gt 0 ]
     then
