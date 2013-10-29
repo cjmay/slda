3d5a1f770f97adb96dba4bb32d00f0d3173a453b
Chandler May <cjmay@jhu.edu>
2013-10-28 20:13:43 -0400

    pass gp dir through latest clone wrapper

--- clone_tng3_qsub_n.sh
+++ clone_tng3_qsub_n.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ $# -eq 1 ]
+if [ $# -eq 2 ]
 then
-    n=$1
+    n=$2
 else
     n=30
 fi
 
 for i in `seq 1 $n`
 do
-    bash clone_tng3.sh qsub -q text.q coe-qsub-job
+    bash clone_tng3.sh "$1" qsub -q text.q coe-qsub-job
 done
