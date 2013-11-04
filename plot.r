48d47f0a083033d8e406ce36d9f5c0028067eb5b
Chandler May <cjmay4754@gmail.com>
2013-11-04 16:34:40 -0500

    plot 3 by ibs

--- plot.r
+++ plot.r
@@ -27,7 +27,6 @@ plot.experiments <- function(experiment.group.name, experiment.names) {
 plot.experiments('1', c('1-rs0', '1-rs1k', '1-rs10k', '1-rs100k'))
 plot.experiments('2', c('2-rs1k-ibs0', '2-rs1k-ibs10', '2-rs1k-ibs100', '2-rs1k-ibs1k'))
 plot.experiments('9', c('9-rs0', '9-rs1k', '9-rs10k', '9-rs100k'))
-plot.experiments('3', c('3-rs0-ibs10', '3-rs0-ibs100', '3-rs0-ibs1k',
-                        '3-rs1k-ibs10', '3-rs1k-ibs100', '3-rs1k-ibs1k',
-                        '3-rs10k-ibs10', '3-rs10k-ibs100', '3-rs10k-ibs1k',
-                        '3-rs100k-ibs10', '3-rs100k-ibs100', '3-rs100k-ibs1k'))
+plot.experiments('3-ibs10', c('3-rs0-ibs10', '3-rs1k-ibs10', '3-rs10k-ibs10', '3-rs100k-ibs10'))
+plot.experiments('3-ibs100', c('3-rs0-ibs100', '3-rs1k-ibs100', '3-rs10k-ibs100', '3-rs100k-ibs100'))
+plot.experiments('3-ibs1k', c('3-rs0-ibs1k', '3-rs1k-ibs1k', '3-rs10k-ibs1k', '3-rs100k-ibs1k'))
