3dd907966e918d06432aa00debedc7bcf61bc5e9
Chandler May <cjmay4754@gmail.com>
2013-11-04 14:09:08 -0500

    sketch new process log

--- process_log.py
+++ process_log.py
@@ -5,56 +5,66 @@ import os.path
 import sys
 
 
-def mean(x):
-    return sum(x) / len(x)
+LOG_STEM = 'slda.o'
 
 
 class LogData(object):
-    def __init__(self, doc_nums, time_per_word, num_words, nmi, num_particles,
-            rejuvenate):
+    def __init__(self, doc_nums, time_per_word, num_words, in_sample_nmi,
+            in_sample_non_init_nmi, out_of_sample_nmi, rejuvenations):
         self.doc_nums = doc_nums
         self.time_per_word = time_per_word
         self.num_words = num_words
-        self.nmi = nmi
-        self.num_particles = num_particles
-        self.rejuvenate = rejuvenate
+        self.in_sample_nmi = in_sample_nmi
+        self.in_sample_non_init_nmi = in_sample_non_init_nmi
+        self.out_of_sample_nmi = out_of_sample_nmi
+        self.rejuvenations = rejuvenations
 
 
 class AggLogData(object):
     def __init__(self):
-        self.mean_nmi = []
+        self.num_runs = 0
+        self.in_sample_nmi = []
 
     def add(self, log_data):
-        if self.mean_nmi:
-            if len(log_data.nmi) == len(self.mean_nmi):
-                for i in range(len(log_data.nmi)):
-                    self.mean_nmi[i].append(mean(log_data.nmi[i]))
+        if self.in_sample_nmi:
+            if len(log_data.in_sample_nmi) == len(self.in_sample_nmi):
+                self.num_runs += 1
+                for i in range(len(log_data.in_sample_nmi)):
+                    self.in_sample_nmi[i].append(log_data.in_sample_nmi[i])
             else:
-                sys.stderr.write('Wrong number of documents: %d != %d\n' % (len(log_data.nmi), len(self.mean_nmi)))
+                sys.stderr.write('Wrong number of documents: %d != %d\n'
+                    % (len(log_data.in_sample_nmi), len(self.in_sample_nmi)))
         else:
-            self.mean_nmi = [[mean(p)] for p in log_data.nmi]
+            self.num_runs += 1
+            self.in_sample_nmi = [[p] for p in log_data.in_sample_nmi]
 
     def write(self, filename):
         with open(filename, 'w') as f:
-            f.write('\t'.join('run.%d' % i for i in range(len(self.mean_nmi[0]))) + '\n')
-            for nmi in self.mean_nmi:
+            f.write('\t'.join('run.%d' % i for i in range(self.num_runs))
+                + '\n')
+            for nmi in self.in_sample_nmi:
                 f.write('\t'.join(str(x) for x in nmi) + '\n')
             
 
 
-def process_logs(log_location, out_filename):
+def process_logs(logs_location, out_filename):
     agg_log_data = AggLogData()
 
-    if os.path.isdir(log_location):
-        for entry in os.listdir(log_location):
-            path = os.path.join(log_location, entry)
-            if os.path.isfile(path):
-                d = parse_log(path)
-                agg_log_data.add(d)
-    else:
-        path = log_location
-        d = parse_log(path)
-        agg_log_data.add(d)
+    if not os.path.isdir(logs_location):
+        raise IllegalArgumentException(logs_location + ' is not a directory')
+    for dataset_entry in os.listdir(logs_location):
+        dataset_path = os.path.join(logs_location, dataset_entry)
+        if not os.path.isdir(dataset_path):
+            raise IllegalArgumentException(dataset_path + ' is not a directory')
+        for run_entry in os.listdir(dataset_path):
+            run_path = os.path.join(dataset_path, run_entry)
+            if not os.path.isdir(run_path):
+                raise IllegalArgumentException(run_path + ' is not a directory')
+            for entry in os.listdir(run_path):
+                path = os.path.join(run_path, entry)
+                if os.path.isfile(path) and entry.startswith(LOG_STEM):
+                    d = parse_log(path)
+                    agg_log_data.add(d)
 
     agg_log_data.write(out_filename)
 
@@ -62,12 +72,13 @@ def process_logs(log_location, out_filename):
 def parse_log(log_filename):
     with open(log_filename) as f:
         doc_num = None
-        rejuvenate = []
+        rejuvenations = []
         time_per_word = []
         num_words = []
-        nmi = []
+        in_sample_nmi = []
+        in_sample_non_init_nmi = []
+        out_of_sample_nmi = []
         doc_nums = []
-        num_particles = None
         for line in f:
             line = line.strip()
             if line.startswith('DOCUMENT'):
@@ -75,24 +86,27 @@ def parse_log(log_filename):
                 doc_nums.append(doc_num)
             elif line.startswith('REJUVENATE'):
                 token = int(line.split()[1])
-                rejuvenate.append((doc_num, token))
+                rejuvenations.append((doc_num, token))
             elif line.startswith('TIMEPERWORD'):
                 ms = int(line.split()[1])
                 time_per_word.append(ms)
             elif line.startswith('NUMWORDS '):
                 num = int(line.split()[1])
                 num_words.append(num)
-            elif line.startswith('Array('):
-                if doc_num is not None:
-                    nmi_by_particle = [float(x) for x in
-                        line[len('Array('):len(line)-1].split(', ')]
-                    nmi.append(nmi_by_particle)
-                    num_particles = len(nmi)
+            elif line.startswith('IN-SAMPLE NMI '):
+                nmi = float(line.split()[2])
+                in_sample_nmi.append(nmi)
+            elif line.startswith('IN-SAMPLE-NON-INIT NMI '):
+                nmi = float(line.split()[2])
+                in_sample_non_init_nmi.append(nmi)
+            elif line.startswith('OUT-OF-SAMPLE NMI '):
+                nmi = float(line.split()[2])
+                out_of_sample_nmi.append(nmi)
             elif line.startswith('WRITE TOPICS'):
                 break
 
-    return LogData(doc_nums, time_per_word, num_words, nmi, num_particles,
-        rejuvenate)
+    return LogData(doc_nums, time_per_word, num_words, in_sample_nmi,
+        in_sample_non_init_nmi, out_of_sample_nmi, rejuvenations)
 
 
 if __name__ == '__main__':
