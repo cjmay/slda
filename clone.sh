54d158c5cf95c5fa7006a4319134f84fe089b0fa
Chandler May <cjmay@jhu.edu>
2013-12-02 16:44:32 -0500

    fix paths?

--- clone.sh
+++ clone.sh
@@ -4,21 +4,14 @@ set -e
 
 ROOT_REPO_PATH="$PWD"
 
-if [ $# -eq 1 ]
+if [ $# -ne 1 ]
 then
-    mkdir -p "$1"
-    NEW_REPO_PATH_STEM="$1/slda-run"
-else
-    NEW_REPO_PATH_STEM="$HOME/slda-run"
+    echo "Specify a repo path." >&2
+    exit 1
 fi
 
-i=0
-new_repo_path="$NEW_REPO_PATH_STEM-$i"
-while [ -d "$new_repo_path" ]
-do
-    i=$(($i + 1))
-    new_repo_path="$NEW_REPO_PATH_STEM-$i"
-done
+mkdir -p "$1"
+new_repo_path="$1"
 
 git clone -q "$ROOT_REPO_PATH" "$new_repo_path"
 ln -s "$ROOT_REPO_PATH/data/20news-bydate-train" "$new_repo_path/data/"
--- clone_tng3_qsub.sh
+++ clone_tng3_qsub.sh
@@ -4,20 +4,23 @@ set -e
 
 if [ $# -ne 1 ]
 then
-    echo 'Specify the repo grandparent dir.' >&2
+    echo 'Specify the repo parent dir.' >&2
     exit 1
 fi
+
+export JAVA_HOME="/export/apps/java/bin/java"
+export PATH="/export/apps/sbt-0.12.1/bin:$PATH"
+export PATH="/export/apps/scala-2.9.2/bin:$PATH"
 
 params_name_stem=PfParams
 params_rel_path=src/main/scala/lda/RunLda.scala
 
-new_repo_grandparent_dir="$1"
+new_repo_parent_dir="$1"
 
 for params_name_prefix in Diff3 Rel3 Sim3
 do
-    new_repo_parent_dir="$new_repo_grandparent_dir/"`echo "$params_name_prefix" | tr A-Z a-z`
-    new_repo_path=`bash clone.sh "$new_repo_parent_dir"`
-    echo "$new_repo_path"
+    new_repo_path="$new_repo_parent_dir/"`echo "$params_name_prefix" | tr A-Z a-z`
+    bash clone.sh "$new_repo_path"
     sed -i '/^[[:space:]]*val params: RunLdaParams =.*$/s/=.*$/'"= ${params_name_prefix}${params_name_stem}/" "$new_repo_path/$params_rel_path"
     pushd "$new_repo_path" >/dev/null
     sbt package
