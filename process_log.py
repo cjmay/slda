76561c3e0c17f37b34b17f603a9a6dd1a17e891d
Chandler May <cjmay4754@gmail.com>
2013-12-12 17:11:00 -0500

    output perplexity, etc in process_log

--- process_log.py
+++ process_log.py
@@ -7,50 +7,72 @@ import sys
 
 LOG_STEM = 'slda.o'
 
+PER_DOC_STAT_NAMES = (
+    'IN-SAMPLE NMI',
+    'IN-SAMPLE-NON-INIT NMI',
+    'OUT-OF-SAMPLE NMI',
+    'OUT-OF-SAMPLE PERPLEXITY',
+    'OUT-OF-SAMPLE LOG-LIKELIHOOD',
+    'TIMEPERWORD',
+)
+
 
 class LogData(object):
-    def __init__(self, doc_nums, time_per_word, num_words, in_sample_nmi,
-            in_sample_non_init_nmi, out_of_sample_nmi, rejuvenations):
+    def __init__(self, doc_nums, rejuvenations, num_words, per_doc_stats):
         self.doc_nums = doc_nums
-        self.time_per_word = time_per_word
-        self.num_words = num_words
-        self.in_sample_nmi = in_sample_nmi
-        self.in_sample_non_init_nmi = in_sample_non_init_nmi
-        self.out_of_sample_nmi = out_of_sample_nmi
         self.rejuvenations = rejuvenations
+        self.num_words = num_words
+        self.per_doc_stats = per_doc_stats
 
 
 class AggLogData(object):
+    BADNESS_RE = re.compile(r'\W')
+
     def __init__(self):
-        self.num_runs = 0
-        self.in_sample_nmi = []
-        self.out_of_sample_nmi = []
+        self.per_doc_stats = dict((stat_name, []) for stat_name in PER_DOC_STAT_NAMES)
+
+    def normalize_stat_name(self, name):
+        return BADNESS_RE.subn('_', name.lower())
+
+    def num_runs(self):
+        if stats:
+            return len(stats[0])
+        else:
+            return 0
+
+    def make_na(self, n):
+        return ['NA' for i in range(n)]
 
     def add(self, log_data):
-        for attr_name in ('in_sample_nmi', 'out_of_sample_nmi'):
-            log_nmi_array = getattr(log_data, attr_name)
-            nmi_array = getattr(self, attr_name)
-            if len(log_nmi_array) == len(nmi_array):
-                if attr_name == 'in_sample_nmi': # don't over-count
-                    self.num_runs += 1
-                for i in range(len(log_nmi_array)):
-                    nmi_array[i].append(log_nmi_array[i])
-            elif len(nmi_array) == 0:
-                if attr_name == 'in_sample_nmi': # don't over-count
-                    self.num_runs += 1
-                for p in log_nmi_array:
-                    nmi_array.append([p])
-            else:
-                sys.stderr.write('Wrong number of documents: %d != %d\n'
-                    % (len(log_nmi_array), len(nmi_array)))
+        for stat_name in per_doc_stats:
+            run_stats = log_data.per_doc_stats[stat_name]
+            stats = self.per_doc_stats[stat_name]
 
-    def write(self, in_sample_filename, out_of_sample_filename):
-        for (filename, nmi_array) in ((in_sample_filename, self.in_sample_nmi),
-                                      (out_of_sample_filename, self.out_of_sample_nmi)):
+            # Adjust for any differences in lengths
+            if len(stats) == 0:
+                # initialize to empty arrays
+                stats += [[] for i in range(len(run_stats)]
+            elif len(run_stats) > len(stats):
+                # pad stats with arrays of NA records
+                pad_len = len(run_stats) - len(stats)
+                stats += [self.make_na(self.num_runs()) for j in range(pad_len)]
+            elif len(run_stats) < len(stats):
+                # pad run_stats with NA records
+                pad_len = len(stats) - len(run_stats)
+                run_stats += self.make_na(pad_len)
+
+            # Add (adjusted) run stats to (adjusted) aggregate stats
+            for i in range(len(run_stats)):
+                stats[i].append(run_stats[i])
+
+    def write(self, filename_template):
+        for stat_name in self.per_doc_stats:
+            stats = self.per_doc_stats[stat_name]
+            filename = filename_template % self.normalize_stat_name(stat_name)
             with open(filename, 'w') as f:
-                f.write('\t'.join('run.%d' % i for i in range(self.num_runs)) + '\n')
-                for nmi in nmi_array:
-                    f.write('\t'.join(str(x) for x in nmi) + '\n')
+                f.write('\t'.join('run.%d' % i for i in range(self.num_runs())) + '\n')
+                for iter_stats in stats:
+                    f.write('\t'.join(str(x) for x in iter_stats) + '\n')
             
 
 
@@ -63,62 +85,44 @@ def process_logs(experiment_path):
         dataset_path = os.path.join(experiment_path, dataset_entry)
         if not os.path.isdir(dataset_path):
             raise Exception(dataset_path + ' is not a directory')
-        for run_entry in os.listdir(dataset_path):
-            run_path = os.path.join(dataset_path, run_entry)
-            if not os.path.isdir(run_path):
-                raise Exception(run_path + ' is not a directory')
-            for entry in os.listdir(run_path):
-                path = os.path.join(run_path, entry)
-                if os.path.isfile(path) and entry.startswith(LOG_STEM):
-                    d = parse_log(path)
-                    agg_log_data.add(d)
-        in_sample_out_filename = dataset_path + '_is.tab'
-        if os.path.exists(in_sample_out_filename):
-            raise Exception(in_sample_out_filename + ' already exists')
-        out_of_sample_out_filename = dataset_path + '_oos.tab'
-        if os.path.exists(out_of_sample_out_filename):
-            raise Exception(out_of_sample_out_filename + ' already exists')
-        agg_log_data.write(in_sample_out_filename, out_of_sample_out_filename)
+        for entry in os.listdir(dataset_path):
+            path = os.path.join(dataset_path, entry)
+            if os.path.isfile(path) and entry.startswith(LOG_STEM):
+                agg_log_data.add(parse_log(path))
+        agg_log_data.write(dataset_path + '_%s.tab')
 
 
 def parse_log(log_filename):
     with open(log_filename) as f:
         doc_num = None
-        rejuvenations = []
-        time_per_word = []
-        num_words = []
-        in_sample_nmi = []
-        in_sample_non_init_nmi = []
-        out_of_sample_nmi = []
         doc_nums = []
+        rejuvenations = []
+        num_words = []
+        per_doc_stats = dict((stat_name, []) for stat_name in PER_DOC_STAT_NAMES)
         for line in f:
             line = line.strip()
-            if line.startswith('DOCUMENT'):
-                doc_num = int(line.split()[1])
+
+            split_idx = line.rfind(' ')
+            if split_idx >= 0:
+                key = line[:split_idx]
+                val = line[split_idx+1:].strip()
+            else:
+                key = None
+                val = None
+
+            if key == 'DOCUMENT':
+                doc_num = int(val)
                 doc_nums.append(doc_num)
-            elif line.startswith('REJUVENATE'):
-                token = int(line.split()[1])
-                rejuvenations.append((doc_num, token))
-            elif line.startswith('TIMEPERWORD'):
-                ms = int(line.split()[1])
-                time_per_word.append(ms)
-            elif line.startswith('NUMWORDS '):
-                num = int(line.split()[1])
-                num_words.append(num)
-            elif line.startswith('IN-SAMPLE NMI '):
-                nmi = float(line.split()[2])
-                in_sample_nmi.append(nmi)
-            elif line.startswith('IN-SAMPLE-NON-INIT NMI '):
-                nmi = float(line.split()[2])
-                in_sample_non_init_nmi.append(nmi)
-            elif line.startswith('OUT-OF-SAMPLE NMI '):
-                nmi = float(line.split()[2])
-                out_of_sample_nmi.append(nmi)
-            elif line.startswith('WRITE TOPICS'):
+            elif key == 'REJUVENATE':
+                rejuvenations.append((doc_num, int(val)))
+            elif key == 'NUMWORDS':
+                num_words.append(int(val))
+            elif key in PER_DOC_STAT_NAMES:
+                per_doc_stats[key].append(float(val))
+            elif line == 'WRITE TOPICS':
                 break
 
-    return LogData(doc_nums, time_per_word, num_words, in_sample_nmi,
-        in_sample_non_init_nmi, out_of_sample_nmi, rejuvenations)
+    return LogData(doc_nums, rejuvenations, num_words, **per_doc_stats)
 
 
 if __name__ == '__main__':
