b518d8a9af47bb1c71be488245aff7b69c9c2f6f
Chandler May <cjmay4754@gmail.com>
2013-12-19 22:06:53 -0500

    eval abstraction for static test set

--- src/main/scala/lda/Evaluation.scala
+++ src/main/scala/lda/Evaluation.scala
@@ -4,23 +4,38 @@
 package evaluation
 
 import lda._
+import stream._
 
+trait Evaluator {
+  def outOfSampleEval(globalVect: GlobalUpdateVector)
+}
 
-class Evaluator(inferentialSampler: InferentialGibbsSampler) {
+class GigawordEvaluator(inferentialSampler: InferentialGibbsSampler,
+    streamHeadBuffer: StreamHeadBuffer) extends Evaluator {
   def outOfSampleEval(globalVect: GlobalUpdateVector): Unit = {
-    val (beforePerplexity, beforeLL, beforeNumWords) = inferentialSampler.beforePerplexity(globalVect)
+    val (beforePerplexity, beforeLL, beforeNumWords) = inferentialSampler.perplexity(streamHeadBuffer.beforeIterator, globalVect)
     println("BEFORE OUT-OF-SAMPLE PERPLEXITY " + beforePerplexity)
     println("BEFORE OUT-OF-SAMPLE LOG-LIKELIHOOD " + beforeLL)
     println("BEFORE OUT-OF-SAMPLE NUMWORDS " + beforeNumWords)
 
-    val (afterPerplexity, afterLL, afterNumWords) = inferentialSampler.afterPerplexity(globalVect)
+    val (afterPerplexity, afterLL, afterNumWords) = inferentialSampler.perplexity(streamHeadBuffer.afterIterator, globalVect)
     println("AFTER OUT-OF-SAMPLE PERPLEXITY " + afterPerplexity)
     println("AFTER OUT-OF-SAMPLE LOG-LIKELIHOOD " + afterLL)
     println("AFTER OUT-OF-SAMPLE NUMWORDS " + afterNumWords)
 
-    val (samplePerplexity, sampleLL, sampleNumWords) = inferentialSampler.samplePerplexity(globalVect)
+    val (samplePerplexity, sampleLL, sampleNumWords) = inferentialSampler.perplexity(streamHeadBuffer.sampleIterator, globalVect)
     println("SAMPLE OUT-OF-SAMPLE PERPLEXITY " + samplePerplexity)
     println("SAMPLE OUT-OF-SAMPLE LOG-LIKELIHOOD " + sampleLL)
     println("SAMPLE OUT-OF-SAMPLE NUMWORDS " + sampleNumWords)
+  }
+}
+
+class StaticEvaluator(inferentialSampler: InferentialGibbsSampler,
+    docs: Array[Array[String]]) extends Evaluator {
+  def outOfSampleEval(globalVect: GlobalUpdateVector): Unit = {
+    val (perplexity, ll, numWords) = inferentialSampler.perplexity(docs.iterator, globalVect)
+    println("OUT-OF-SAMPLE PERPLEXITY " + perplexity)
+    println("OUT-OF-SAMPLE LOG-LIKELIHOOD " + ll)
+    println("OUT-OF-SAMPLE NUMWORDS " + numWords)
   }
 }
